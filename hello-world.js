287c388e3703e81db93935baf3a4d865499bd7ae
Bryan LeBlanc <mrbeardbaron@gmail.com>
2017-01-16 19:44:01 +0000

    question 2

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,20 @@
 function helloWorld() {
-    console.log("Hello World!");
-    setTimeout(function() {
-    console.log("Hello World Again!");
-}, 10000)
+  console.log("Hello World!");
+  setTimeout(function() {
+  console.log("Hello World Again!");
+}, 3000)
 }
 helloWorld();
+
+function helloWorld10() {
+  console.log("Hello World");
+    setTimeout(hello10, 3000);
+}
+setTimeout(hello10, 3000);
+
+function helloWorld11() {
+  setInterval(function() {
+    console.log("Hello World!");
+  }, 3000);
+}
+helloWorld11();
